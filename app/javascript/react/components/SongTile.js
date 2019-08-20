fd5d7e53a32722b487d62b23924d6ed2bd5f473e
Richard Holway <rgholway@syr.edu>
2019-08-19 20:14:31 -0400

    Change for push

--- app/javascript/react/components/SongTile.js
+++ app/javascript/react/components/SongTile.js
@@ -7,29 +7,23 @@ class SongTile extends Component {
         youtube: ""
         }
         this.onClick = this.onClick.bind(this)
-        this.playNext = this.playNext.bind(this)
   }
 
     onClick(e) {
       if (e.type == "click") {
         this.props.onClick(this.props.id)
         }
-      if (e.type == "contextmenu") {
-        this.props.onNext(this.props.youtube)
+      if (e.nativeEvent.which == 3) {
+        e.preventDefault()
+        this.props.onNext(this.props.youtube, this.props.title)
       }
 
-    }
-
-    playNext(e) {
-      e.preventDefault()
-      this.props.onNext(this.props.youtube, this.props.title)
     }
 
   render() {
     return (
       <div>
-        <div className="tracks" onContextMenu={ this.playNext} onClick={this.onClick}>{this.props.num}. {this.props.title}
-          <div className="play__next" onClick={this.playNext}>O</div>
+        <div className="tracks" onContextMenu={this.onClick} onClick={this.onClick}>{this.props.num}. {this.props.title}
         </div>
       </div>
     )
