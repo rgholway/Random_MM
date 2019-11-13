7d6dc4f6f28bcac0876e1f0c23bc9bb3a1297089
Richard Holway <rgholway@syr.edu>
2019-11-13 18:12:30 -0500

    Sound is playing just have to get mp3s into the file tree

--- app/javascript/react/components/Vote.js
+++ app/javascript/react/components/Vote.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { browserHistory } from 'react-router';
 import {Link} from 'react-router';
-import song from '../../../assets/audio/New__Faces.mp3'
 
 class Vote extends Component {
   constructor(props) {
@@ -9,17 +8,14 @@ class Vote extends Component {
     this.state = {
       song: ""
     }
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick() {
-    this.setState({ song: '../../../assets/audio/New__Faces.mp3'})
   }
 
   render() {
     return(
-      <div className="vote" onClick={this.onClick()}>
-        <audio ref={ref => this.state.song = ref} />
+      <div className="vote">
+        <audio controls autoPlay>
+          <source src="https://s3.amazonaws.com/freecodecamp/simonSound1.mp3" type="audio/mpeg"/>
+        </audio>
       </div>
     )}
   }
