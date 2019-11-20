acf419c22ed283e61699a054b4c5374a09176e35
Richard Holway <rgholway@syr.edu>
2019-11-19 21:37:05 -0500

    Starting Votes

--- db/migrate/20190608173951_songs.rb
+++ db/migrate/20190608173951_songs.rb
@@ -3,6 +3,9 @@ class Songs < ActiveRecord::Migration[5.2]
   create_table :songs do |t|
     t.string :name, null: false
     t.string :youtube, null: false
+    t.string :vote
+    t.string :first_characteristic
+    t.string :second_characteristic
 
     t.belongs_to :album
 
