f0ebcbed02b060aba1ab448a2722156e7747a1cd
Richard Holway <rgholway@syr.edu>
2019-12-11 21:35:12 -0500

    Songs showup and play

--- app/javascript/react/components/ShowPlaylist.js
+++ app/javascript/react/components/ShowPlaylist.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
-import VoteTile from './VoteTile';
+import PlaylistTile from './PlaylistTile';
+import VoteVideo from './VoteVideo';
 
 class PlaylistShow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      playlist: ""
+      playlist: [],
+      youtube: "",
+      id: "",
+      setting: "--play--active",
+      play: ""
         }
       this.fetchPlaylist = this.fetchPlaylist.bind(this)
+      this.handleClick = this.handleClick.bind(this)
+      this.handleStop = this.handleStop.bind(this)
+      this.stopSong = this.stopSong.bind(this)
   }
 
   fetchPlaylist() {
@@ -28,17 +36,55 @@ class PlaylistShow extends Component {
     })
   }
 
+  handleClick(id, youtube) {
+    this.setState({ id: id, youtube: youtube})
+  }
+
+  stopSong() {
+    this.setState({ play: "" })
+  }
+
+  handleStop() {
+    if (this.state.setting == "--play--active") {
+      this.setState({ setting: "--stop--active" })
+      this.stopSong()
+    }
+    if (this.state.setting == "--stop--active") {
+      this.setState({ setting: "--play--active" })
+    }
+  }
+
   componentWillMount() {
     this.fetchPlaylist()
   }
 
   render() {
+    console.log(this.state.setting);
+    let playlistArray = this.state.playlist.map(playlist => {
+      return(
+        <PlaylistTile
+          key= {playlist[0]}
+          id= {playlist[0]}
+          name= {playlist[1]}
+          youtube= {playlist[2]}
+          onClick= {this.handleClick}
+        />
+      )
+    })
     return (
       <div className="vote">
+        <div className="play__button--playlist" onClick={this.handleStop}>
+          <div className={`play__button${this.state.setting}`} ></div>
+          <div className={`play__button${this.state.setting}`}></div>
+        </div>
           <div className="song__playlist">
             <div className="song__playlist__title"> First Playlist </div>
-            
+            {playlistArray}
           </div>
+          <VoteVideo
+            key= {this.state.id}
+            youtube = {this.state.youtube}
+          />
       </div>
     )
   }
